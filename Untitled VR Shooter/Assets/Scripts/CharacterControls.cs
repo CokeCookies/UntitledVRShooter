6d80932bff9b16c05632cdcf40a179dba4f2d4ad
JrSauceBottle <cjkrone19@gmail.com>
2021-06-04 14:38:32 +1000

    Callum:- Started adding character controls

---
+++ Untitled VR Shooter/Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour
+{
+    [SerializeField]
+    private float playerHealth = 5;
+
+    [SerializeField] 
+    private AudioClip clip;
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private Transform gunBarrelTransform;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+    }
+
+    private void Update()
+    {
+        Input();
+    }
+
+    private void Input()
+    {
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
+        {
+            audioSource.Play();
+            RaycastFromGunBarrel();
+        }
+    }
+
+    private void RaycastFromGunBarrel()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(gunBarrelTransform.position, gunBarrelTransform.forward, out hit))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                //Call Enemy hit function here
+
+                //Temporary Code
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+
+    public float PlayerTakeDamage(float damage)
+    {
+        playerHealth = playerHealth - damage;
+        return playerHealth;
+    }
+}
